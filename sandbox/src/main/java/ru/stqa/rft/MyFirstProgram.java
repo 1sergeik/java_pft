e74ee751a71cb86cf69d87f12f627a5ec409f92f
1sergeik <1sergeik@gmail.com>
2019-07-09 21:20:27 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/rft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/rft/MyFirstProgram.java
@@ -2,7 +2,23 @@ package ru.stqa.rft;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+    Hello("world");
+    Hello("Alexei");
+    double ll=5;
+    System.out.println("Площадь квадрата со стороной "+ll+"="+area(ll));
 
-    System.out.println("Hello, world!");
+    double a=4;
+    double b=6;
+    System.out.println("Площадь прямоугольника со сторона "+a+" и "+b+" равна "+area(a,b));
+
+  }
+  public static void Hello(String somebody) {
+    System.out.println("Hello,"+somebody+"!");
+  }
+  public static double area(double len){
+    return len*len;
+  }
+  public static double area(double a, double b){
+    return a*b;
   }
 }
