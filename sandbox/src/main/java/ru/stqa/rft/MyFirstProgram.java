c6e07a6517b4c8490dd74d13a449c3342fd3b1f7
1sergeik <1sergeik@gmail.com>
2019-07-20 14:00:27 +0300

    Задание 2
    Задание 2

--- sandbox/src/main/java/ru/stqa/rft/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/rft/MyFirstProgram.java
@@ -5,20 +5,28 @@ public class MyFirstProgram {
     Hello("world");
     Hello("Alexei");
 
-    Square s= new Square(5);
+    Square s = new Square(5);
 
-    System.out.println("Площадь квадрата со стороной "+s.l+"="+s.area());
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.area());
 
-    Rectangle r = new Rectangle(4,6);
+    Rectangle r = new Rectangle(4, 6);
 
-    System.out.println("Площадь прямоугольника со сторона "+r.a+" и "+r.b+" равна "+r.area());
+    System.out.println("Площадь прямоугольника со сторона " + r.a + " и " + r.b + " равна " + r.area());
+
+    Point p = new Point(-2,2,1,-2);
+
+    System.out.println("расстояние между точками p1 b p2 с координатами ("+p.x1+";"+p.y1+") и ("+p.x2+";"+p.y2+") равна " + p.distance());
 
   }
+
   public static void Hello(String somebody) {
-    System.out.println("Hello,"+somebody+"!");
+    System.out.println("Hello," + somebody + "!");
   }
-  public static double area(Square s){
-    return s.l*s.l;
+
+  public static double area(Square s) {
+    return s.l * s.l;
   }
+
 
 }
+
