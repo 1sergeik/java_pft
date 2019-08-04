dd9e7f8e924d782484c8f6c596cf001cd07cb367
1sergeik <1sergeik@gmail.com>
2019-08-05 00:11:19 +0300

    задание 3
    задание 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.rft.P1;
+import ru.stqa.rft.Point;
 
 public class PointTest {
 
@@ -10,9 +10,11 @@ public class PointTest {
 
   public void testDistance() {
 
-    P1 p;
-    p = new P1(3,-1,0,3);
-    Assert.assertEquals(p.distance(),5.0);
+    Point p1;
+    p1 = new Point(3,-1);
+    Point p2;
+    p2 = new Point (0,3);
+    Assert.assertEquals(p1.distance(p1,p2),5.0);
 
 
   }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest1.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest1.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.rft.Point;
 
-public class PointTest2 {
+public class PointTest1 {
 
   @Test
 
@@ -13,8 +13,8 @@ public class PointTest2 {
     Point p1;
     p1 = new Point(-5,8);
     Point p2;
-    p2 = new Point (-14,20);
-    Assert.assertEquals(p1.distance(p1,p2),15.0);
+    p2 = new Point (+1,0);
+    Assert.assertEquals(p1.distance(p1,p2),10.0);
 
 
   }
